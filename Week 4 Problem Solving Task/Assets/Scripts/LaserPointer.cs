ec03267cbefbf8ce3ad96d3a2d23748eceafc987
Ben Marriner <bmarriner@deakin.edu.au>
2022-08-12 03:21:07 +1000

    Finished week 4 task using HTC VIVE

---
+++ Week 4 Problem Solving Task/Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LaserPointer : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean laserAction;
+    public GameObject laserPrefab;
+    private GameObject laser;
+    private Transform laserTransform;
+    private Vector3 hitPoint;
+    private GameObject hitObject;
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(controllerPose.transform.position, hitPoint, 0.5f);
+        laserTransform.LookAt(hitPoint);
+        laserTransform.localScale = new Vector3
+        (
+            laserTransform.localScale.x,
+            laserTransform.localScale.y,
+            hit.distance
+        );
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (laserAction.GetState(handType))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100))
+            {
+                hitPoint = hit.point;
+                hitObject = hit.transform.gameObject;
+                InteractableObject interactableObject = hitObject.GetComponent<InteractableObject>();
+                if (interactableObject)
+                {
+                    interactableObject.Interact();
+                }
+                ShowLaser(hit);
+            }
+
+            print("Firing laser");
+        }
+        else laser.SetActive(false);
+    }
+}
